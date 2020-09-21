867ec4a93f4f932cb020a8284ba6047831ab7c02
sun bo <sunbo32@huawei.com>
2020-09-21 20:32:44 +0800

    change 2d map to costmap

--- nav2_3d_costmap_plugin/include/nav2_3d_costmap_plugin/nav2_3d_costmap.hpp
+++ nav2_3d_costmap_plugin/include/nav2_3d_costmap_plugin/nav2_3d_costmap.hpp
@@ -8,6 +8,7 @@
 #include "nav2_costmap_2d/obstacle_layer.hpp"
 #include "nav2_costmap_2d/costmap_layer.hpp"
 #include "nav2_costmap_2d/layered_costmap.hpp"
+#include "nav2_costmap_2d/layer.hpp"
 #include "nav2_costmap_2d/observation_buffer.hpp"
 #include "nav2_costmap_2d/footprint.hpp"
 #include "nav2_costmap_2d/costmap_2d.hpp"
@@ -30,21 +31,30 @@ class Nav23dStaticLayer : public nav2_costmap_2d::ObstacleLayer
             virtual void reset();
 
             virtual void convertTo2D(sensor_msgs::msg::PointCloud2);
+            virtual void cloudCallback(sensor_msgs::msg::PointCloud2);
+
+            virtual void updateMap();
 
             virtual void updateBounds(
                 double robot_x, double robot_y, double robot_yaw, double * min_x,
                 double * min_y, double * max_x, double * max_y);
 
-//            virtual void updateCosts(
-//                nav2_costmap_2d::Costmap2D & master_grid,
-//                int min_i, int min_j, int max_i, int max_j);
+            virtual void updateCosts(
+                nav2_costmap_2d::Costmap2D & master_grid,
+                int min_i, int min_j, int max_i, int max_j);
 
             virtual void matchSize();
+
+            virtual void updateMap();
 
             void PointCloud2Callback(
                 sensor_msgs::msg::PointCloud2::ConstSharedPtr message,
                 const std::shared_ptr<nav2_costmap_2d::ObservationBuffer> & buffer);
         private:
+
+//        temp param
+            std::string file_path_;
+
             bool rolling_window_;
             std::string global_frame_;
             std::string topic_name_;
@@ -52,7 +62,13 @@ class Nav23dStaticLayer : public nav2_costmap_2d::ObstacleLayer
 //            std::shared_ptr<nav2_costmap_2d::Costmap2D> map_2d_;
             nav2_costmap_2d::Costmap2D map_2d_;
             int mark_threshold_;
-            std::vector<std::shared_ptr<message_filters::SubscriberBase>> observation_subscribers_;
+            unsigned char lethal_threshold_;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr msg_subscriber_;
+        double map_resolution_;
+        unsigned char origin_x_ = 0;
+        unsigned char origin_y_ = 0;
+
+        std::vector<std::shared_ptr<message_filters::SubscriberBase>> observation_subscribers_;
             std::vector<std::shared_ptr<tf2_ros::MessageFilterBase>> observation_notifiers_;
             std::vector<std::shared_ptr<nav2_costmap_2d::ObservationBuffer>> observation_buffers_;
 
--- nav2_3d_costmap_plugin/src/nav2_3d_costmap.cpp
+++ nav2_3d_costmap_plugin/src/nav2_3d_costmap.cpp
@@ -1,9 +1,12 @@
 
 
 #include "nav2_3d_costmap_plugin/nav2_3d_costmap.hpp"
-#include "nav2_costmap_2d/costmap_layer.hpp"
-#include "nav2_costmap_2d/layer.hpp"
 #include "pluginlib/class_list_macros.hpp"
+#include "octomap/octomap.h"
+#include "nav2_costmap_2d/layer.hpp"
+#include "nav2_costmap_2d/costmap_layer.hpp"
+#include "nav2_costmap_2d/layered_costmap.hpp"
+
 
 using nav2_costmap_2d::NO_INFORMATION;
 using nav2_costmap_2d::LETHAL_OBSTACLE;
@@ -17,149 +20,117 @@ namespace nav2_3d_costmap_plugin
 
 Nav23dStaticLayer::~Nav23dStaticLayer(){}
 
-// parameters declares
-void
-Nav23dStaticLayer::getParameters()
-{
-    declareParameter("enabled", rclcpp::ParameterValue(true));
-    declareParameter("mark_threshold", rclcpp::ParameterValue(0));
-    declareParameter("topic_name", rclcpp::ParameterValue(""));
-//    declareParameter("", rclcpp::ParameterValue());
-
-// user node_ as replacement
-// <<<<< put try-catch here >>>>>
-    auto node = node_.lock();
-
-//    std::string topics_string;
-
-    node ->get_parameter(name_ + "." + "enabled", enabled_);
-    node ->get_parameter(name_ + "." + "mark_threshold", mark_threshold_);
-    node ->get_parameter(name_ + "." + "topic_name", topic_name_);
-//        node ->get_parameter(name_ + "." + "observation_sources", topics_string);
-
-    rolling_window_ = layered_costmap_->isRolling();
-    default_value_ = NO_INFORMATION;
-    global_frame_ = layered_costmap_->getGlobalFrameID();
-
-//    std::stringstream ss(topics_string);
-//    std::string source;
-//    while (ss >> source)
-//    {
-//        std::string topic;
-//        declareParameter(source + "." + "topic",rclcpp::ParameterValue(source));
-//        node->get_parameter(name_ + "." + source + "." + "  topic", topic);
-//    }
-
-}
 // read parameters in initialize func
 void
 Nav23dStaticLayer::onInitialize()
 {
     nav2_costmap_2d::ObstacleLayer::onInitialize();
-    getParameters();
+
+    declareParameter("enabled", rclcpp::ParameterValue(true));
+    declareParameter("mark_threshold", rclcpp::ParameterValue(0));
+    declareParameter("topic_name", rclcpp::ParameterValue(""));
+    declareParameter("lethal_threshold", rclcpp::ParameterValue(0));
+    declareParameter("map_resolution", rclcpp::ParameterValue(0.0));
+//  <<<<<<<for test>>>>>>
+    declareParameter("file_path", rclcpp::ParameterValue(""));
+
+    auto node = node_.lock();
+    if (!node) {
+        throw std::runtime_error{"Failed to lock node"};
+    }
+
+    node ->get_parameter(name_ + "." + "enabled", enabled_);
+    node ->get_parameter(name_ + "." + "mark_threshold", mark_threshold_);
+    node ->get_parameter(name_ + "." + "topic_name", topic_name_);
+    node ->get_parameter(name_ + "." + "lethal_threshold", lethal_threshold_);
+    node ->get_parameter(name_ + "." +"map_resolution", map_resolution_);
+
+    node ->get_parameter(name_ + "." + "file_path", file_path_);
+
+    rolling_window_ = layered_costmap_->isRolling();
+    default_value_ = NO_INFORMATION;
+    global_frame_ = layered_costmap_->getGlobalFrameID();
 //<<<<< qos settings should be changed >>>>>>
-    rmw_qos_profile_t custom_qos_profile = rmw_qos_profile_sensor_data;
-    custom_qos_profile.depth = 50;
 
-//    subscriber part
-    std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::PointCloud2>> sub(
-        new message_filters::Subscriber<sensor_msgs::msg::PointCloud2>(rclcpp_node_, topic_name_, custom_qos_profile));
+    rclcpp::QoS map_qos(10);  // initialize to default
 
-    std::shared_ptr<tf2_ros::MessageFilter<sensor_msgs::msg::PointCloud2>> filter(
-        new tf2_ros::MessageFilter<sensor_msgs::msg::PointCloud2>(*sub, *tf_, global_frame_, 50, rclcpp_node_));
-
-    filter->registerCallback(
-        std::bind(&Nav23dStaticLayer::PointCloud2Callback, this, std::placeholders::_1, observation_buffers_.back()));
-
+    //    read from buffer once not continuous
+    msg_subscriber_ = node->create_subscription<sensor_msgs::msg::PointCloud2>(
+            topic_name_,
+            map_qos,
+            std::bind(&Nav23dStaticLayer::cloudCallback, this, std::placeholders::_1));
 }
 
 void
-Nav23dStaticLayer::PointCloud2Callback(
-        sensor_msgs::msg::PointCloud2::ConstSharedPtr message,
-        const std::shared_ptr<nav2_costmap_2d::ObservationBuffer> & buffer)
+Nav23dStaticLayer::cloudCallback(sensor_msgs::msg::PointCloud2 pointcloud)
 {
-    pcl::PCLPointCloud2::Ptr cloud_pcl(new pcl::PCLPointCloud2());
-    pcl::PCLPointCloud2::Ptr cloud_filtered(new pcl::PCLPointCloud2());
-    pcl_conversions::toPCL( *message, *cloud_pcl);
-    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-    sor.setInputCloud(cloud_pcl);
-    sor.setFilterFieldName("z");
-    sor.setFilterLimits(0, max_obstacle_height_);
-    sor.setDownsampleAllData(true);
-    sor.setLeafSize(0.01f, 0.01f, 0.01f);
-    sor.filter(*cloud_filtered);
-    sensor_msgs::msg::PointCloud2::SharedPtr filtered_msg;
-    pcl_conversions::fromPCL(*cloud_filtered, *filtered_msg);
+//        add mutex?
+    nav2_costmap_2d::Costmap2D * master = layered_costmap_->getCostmap();
+//    get master size
+    unsigned  int master_x = master->getSizeInCellsX();
+    unsigned  int master_y = master->getSizeInCellsY();
 
-    buffer->lock();
-    buffer->bufferCloud(*filtered_msg);
-    buffer->unlock();
-}
+    map_2d_ = nav2_costmap_2d::Costmap2D(master_x, master_y, map_resolution_, origin_x_, origin_y_);
+    convertTo2D(pointcloud);
+    updateMap();
+    }
+
 
 void Nav23dStaticLayer::convertTo2D(sensor_msgs::msg::PointCloud2 cloud)
 {
-//
-// the process of convert sensor_msgs -> costmap2d
-    unsigned int pc_width = cloud.width;
-    unsigned int pc_length = cloud.height;
-//    resize layered costmap
+    pcl::PointCloud<pcl::PointXYZ> cloud_xyz;
+    pcl::PCLPointCloud2 cloud_pcl;
+    pcl_conversions::toPCL(cloud, cloud_pcl);
+    pcl::fromPCLPointCloud2(cloud_pcl, cloud_xyz);
 
+    octomap::OcTree treeTest (0.1);
+    for (auto p:cloud_xyz.points)
+    {
+        treeTest.updateNode(octomap::point3d(p.x*0.8f, p.y*0.8f, p.z*0.8f), true);
+    }
+    treeTest.updateInnerOccupancy();
 
-// iterator
-    int mark_threshold_ = 10;
-    sensor_msgs::PointCloud2ConstIterator<float> iter_x(cloud, "x");
-    sensor_msgs::PointCloud2ConstIterator<float> iter_y(cloud, "y");
-    sensor_msgs::PointCloud2ConstIterator<float> iter_z(cloud, "z");
-    unsigned int point_x = static_cast<unsigned int>(*iter_x);
-    unsigned int point_y = static_cast<unsigned int>(*iter_y);
-    unsigned int point_z = static_cast<unsigned int>(*iter_z);
-//    simple convert process
-    for (; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z) {
-        unsigned int index = getIndex(point_x, point_y);
-        if (point_z >= mark_threshold_){
-            costmap_[index] = LETHAL_OBSTACLE;
-            map_2d_.setCost(point_x, point_y, LETHAL_OBSTACLE);
-        }else if(point_z < mark_threshold_){
-            costmap_[index] = FREE_SPACE;
-            map_2d_.setCost(point_x, point_y, FREE_SPACE);
+//    map_2d_ = nav2_costmap_2d::Costmap2D(300,300,0.05,0,0);
+
+    std::vector<octomap::point3d> data;
+    for(octomap::OcTree::iterator it = treeTest.begin(); it != treeTest.end(); ++it){
+        if(treeTest.isNodeOccupied(*it))
+        {
+            auto coord = it.getCoordinate();
+
+            int point_x = (int)coord.x() + 100;
+            int point_y = (int)coord.y() + 100;
+            int point_z = (int)coord.z();
+            if (point_z > 5) {
+                map_2d_.setCost(point_x, point_y, LETHAL_OBSTACLE);
+            }else if(point_z < 5){
+                map_2d_.setCost(point_x, point_y, FREE_SPACE);
+            }
+            data.push_back(coord);
         }
     }
 }
 
-
+void
+Nav23dStaticLayer::updateMap()
+{
+    costmap_ = map_2d_.getCharMap();
+}
 
 void
 Nav23dStaticLayer::updateBounds(
     double /*robot_x*/, double /*robot_y*/, double /*robot_yaw*/, double * min_x,
     double * min_y, double * max_x, double * max_y)
 {
-
     if (!enabled_) {
         return;
     }
-    std::vector<nav2_costmap_2d::Observation> observation;
-    if (getMarkingObservations(observation))
-    {
-        for (std::vector<nav2_costmap_2d::Observation>::const_iterator it = observation.begin(); it != observation.end();
-             ++it)
-        {
-            const nav2_costmap_2d::Observation & obs = *it;
-            const sensor_msgs::msg::PointCloud2 & cloud = *(obs.cloud_);
-            convertTo2D(cloud);
-        }
-
-    }
 }
 
-//void
-//Nav23dStaticLayer::updateCosts(
-//    nav2_costmap_2d::Costmap2D & master_grid,
-//    int min_i, int min_j, int max_i, int max_j)
-//{
-//}
-
 void
-Nav23dStaticLayer::matchSize()
+Nav23dStaticLayer::updateCosts(
+    nav2_costmap_2d::Costmap2D & master_grid,
+    int min_i, int min_j, int max_i, int max_j)
 {
 
 }
